c905e9e1479c79b0dfeca80fcadb24803f77e272
Ari Höysniemi <ari.hoysniemi@gmail.com>
2023-10-03 13:53:18 +0000

    Improved texts to reflect aad.

--- src/client/strings/strings.login.ts
+++ src/client/strings/strings.login.ts
@@ -1,15 +1,15 @@
 const en = {
   h1: 'Welcome!',
   h2: 'You are not logged in.',
-  p: 'Login is only possible if your user has been activated.',
-  a_login: 'Login with Google',
+  p: 'Access to the service requires logging in with your organization\'s credentials.',
+  a_login: 'Login with Single Sign-On',
 };
 
 const fi: typeof en = {
   h1: 'Tervetuloa!',
   h2: 'Et ole kirjautunut sisään.',
-  p: 'Sisäänkirjautuminen onnistuu vain, jos käyttäjäsi on aktivoitu.',
-  a_login: 'Kirjaudu Google-tunnuksilla',
+  p: 'Palveluun pääsy vaatii kirjautumisen organisaatiosi tunnuksilla.',
+  a_login: 'Kirjaudu kertakirjautumisella',
 };
 
 export default { en, fi };
